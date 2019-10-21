246046eceff419edbcac155381e3abaef17eaae6
Karl Shultz <kshultz@cloudbees.com>
2019-10-21 15:28:35 -0400

    Add -q to yum commands. Add a keys dir to gitignore.

--- aws/instance-preconfigure.sh
+++ aws/instance-preconfigure.sh
@@ -3,10 +3,12 @@
 set -xe
 set -o pipefail
 
-sudo yum install -y git
-sudo yum install -y docker && sudo service docker start
+sudo yum install -y -q git
+sudo yum install -y -q docker && sudo service docker start
 sudo chkconfig docker on
 sudo groupadd docker || echo "Group already exists"
 sudo usermod -aG docker $USER
 
-# Add Maven installation?
+# Install Java
+sudo yum install -y -q java-1.8.0-openjdk.x86_64
+sudo yum install -y -q java-sdk
